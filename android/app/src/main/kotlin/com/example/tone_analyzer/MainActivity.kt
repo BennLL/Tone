7ecd760e2c9261d756d1e516c5651693681f9063
BenLL <zihaoluo153@gmail.com>
2023-06-11 19:02:22 -0700

    Commit

---
+++ android/app/src/main/kotlin/com/example/tone_analyzer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tone_analyzer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/routepage.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class RoutePage extends StatefulWidget {
+  const RoutePage({super.key});
+
+  @override
+  State<RoutePage> createState() => _RoutePageState();
+}
+
+class _RoutePageState extends State<RoutePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: const Text("Route Page"),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.analytics_outlined),
+            label: "Skin",
+          ),
+        ],
+      ),
+    );
+  }
+}
