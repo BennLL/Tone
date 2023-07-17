adb2fa1470d45076ab9f21f1351433a2b59f84dd
BenLL <zihaoluo153@gmail.com>
2023-07-16 19:40:13 -0700

    progress

--- lib/result.dart
+++ lib/result.dart
@@ -1,17 +1,179 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'dart:convert';
+import 'package:image_picker/image_picker.dart';
+import 'package:http/http.dart' as http;
+import 'package:http_parser/http_parser.dart';
 
 class ResultPage extends StatefulWidget {
-  const ResultPage({super.key});
+  ResultPage({
+    Key? key,
+    required this.image,
+  }) : super(key: key);
+  File image;
 
   @override
   State<ResultPage> createState() => _ResultPageState();
 }
 
 class _ResultPageState extends State<ResultPage> {
+  List? _result;
+
+  void uploadFileToServer() async {
+    var url = '';
+    Map<String, String> headers = {
+      "connection": "Keep-Alive",
+      "Keep-Alive": "timeout=5, max=1000",
+    };
+
+    http.MultipartRequest request =
+        http.MultipartRequest('POST', Uri.parse('$url/fetch_skin_tone'));
+    request.headers.addAll(headers);
+    request.files.add(
+      await http.MultipartFile.fromPath(
+        'image',
+        widget.image.path,
+        contentType: MediaType('application', 'jpeg'),
+      ),
+    );
+
+    request.send().then((r) async {
+      if (r.statusCode == 200) {
+        var result = json.decode(await r.stream.transform(utf8.decoder).join());
+
+        setState(() {
+          _result = result;
+        });
+      }
+    });
+  }
+
+  Padding buildImageContainer(double width, double height) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 12.0),
+      child: Container(
+        width: width,
+        height: height * 0.4,
+        decoration: const ShapeDecoration(
+          shape: RoundedRectangleBorder(
+            side: BorderSide(width: 1.0, style: BorderStyle.solid),
+            borderRadius: BorderRadius.all(Radius.circular(20)),
+          ),
+        ),
+        child: widget.image == null
+            ? Center(
+                child: Text('Image error'),
+              )
+            : Stack(children: [
+                Container(
+                  width: width,
+                  height: height * 0.4,
+                  decoration: const ShapeDecoration(
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(width: 1.0, style: BorderStyle.solid),
+                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Image.file(
+                      File(widget.image!.path),
+                      fit: BoxFit.fitHeight,
+                    ),
+                  ),
+                ),
+              ]),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Result Page")
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 234, 214, 225),
+      appBar: AppBar(
+        elevation: 0,
+        automaticallyImplyLeading: false,
+        backgroundColor: Color.fromARGB(255, 245, 229, 141),
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(50.0),
+            bottomRight: Radius.circular(50.0),
+          ),
+        ),
+        toolbarHeight: 100,
+        flexibleSpace: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Container(
+                height: 85,
+                width: 110,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('images/logo_transparent.png'),
+                    fit: BoxFit.fitHeight,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: _result != null
+          ? Center(
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Column(
+                  children: [
+                    buildImageContainer(width, height),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                            height: 50,
+                            width: 50,
+                            color: Color.fromRGBO(_result![0][0],
+                                _result![0][1], _result![0][2], 1)),
+                        Column(
+                          children: [
+                            Text(
+                              textAlign: TextAlign.left,
+                              'Tone: ${_result![1].split('_')[0].toUpperCase()}',
+                              style: const TextStyle(fontSize: 25),
+                            ),
+                            Text(
+                              textAlign: TextAlign.left,
+                              'Undertone:  ${_result![1].split('_')[1].toUpperCase()}',
+                              style: const TextStyle(fontSize: 25),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            )
+          : Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: const [
+                  CircularProgressIndicator(),
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text("Analyzing Image"),
+                  ),
+                ],
+              ),
+            ),
     );
   }
 }
--- lib/undertone.dart
+++ lib/undertone.dart
@@ -12,6 +12,164 @@ class ToneUnderTonePage extends StatefulWidget {
 }
 
 class _ToneUnderTonePageState extends State<ToneUnderTonePage> {
+  final ImagePicker _picker = ImagePicker();
+  File? _image;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  _imgFromCamera() async {
+    final image =
+        await _picker.pickImage(source: ImageSource.camera, imageQuality: 50);
+
+    setState(() {
+      _image = File(image!.path);
+    });
+  }
+
+  _imgFromGallery() async {
+    final image =
+        await _picker.pickImage(source: ImageSource.gallery, imageQuality: 50);
+
+    setState(() {
+      _image = File(image!.path);
+    });
+  }
+
+  void _showPicker() {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext bc) {
+        return SafeArea(
+          child: Container(
+            child: Wrap(
+              children: <Widget>[
+                ListTile(
+                  leading: const Icon(Icons.photo_library),
+                  title: const Text('Photo Library'),
+                  onTap: () {
+                    _imgFromGallery();
+                    Navigator.of(context).pop();
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.photo_camera),
+                  title: const Text('Camera'),
+                  onTap: () {
+                    _imgFromCamera();
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Future<void> _showMyDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Image.asset(
+            'images/replace.png',
+            height: 100,
+          ),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                const Text("Do You want to change the current Image?"),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                _image = null;
+                Navigator.of(context).pop();
+                _showPicker();
+              },
+              child: const Text("yes"),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text("No"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Padding buildImageContainer(double width, double height) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 12.0),
+      child: Container(
+        width: width,
+        height: height * 0.4,
+        decoration: const ShapeDecoration(
+          shape: RoundedRectangleBorder(
+            side: BorderSide(width: 1.0, style: BorderStyle.solid),
+            borderRadius: BorderRadius.all(Radius.circular(20)),
+          ),
+        ),
+        child: _image == null
+            ? IconButton(
+                icon: const Icon(
+                  Icons.camera_alt_outlined,
+                  size: 80,
+                ),
+                onPressed: () {
+                  _showPicker();
+                },
+              )
+            : Stack(
+                children: [
+                  Container(
+                    width: width,
+                    height: height * 0.4,
+                    decoration: const ShapeDecoration(
+                      shape: RoundedRectangleBorder(
+                        side: BorderSide(width: 1.0, style: BorderStyle.solid),
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Image.file(
+                        File(_image!.path),
+                        fit: BoxFit.fitHeight,
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 5,
+                    right: 0,
+                    child: MaterialButton(
+                      onPressed: () {
+                        _showMyDialog();
+                      },
+                      color: const Color.fromRGBO(31, 150, 247, 1),
+                      child: Icon(
+                        Icons.edit,
+                        size: 24,
+                      ),
+                      shape: CircleBorder(),
+                    ),
+                  ),
+                ],
+              ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
@@ -59,17 +217,28 @@ class _ToneUnderTonePageState extends State<ToneUnderTonePage> {
                   SizedBox(
                     height: 10,
                   ),
+                  buildImageContainer(width, height),
                   Padding(
                     padding: const EdgeInsets.only(top: 20),
-                    child: ElevatedButton(
-                      onPressed: null,
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Color.fromRGBO(50, 50, 50, 1)),
-                      child: const Text(
-                        "Analyze",
-                        style: TextStyle(fontSize: 25),
-                      ),
-                    ),
+                    child: _image == null
+                        ? null
+                        : ElevatedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                CupertinoPageRoute(
+                                  builder: (context) =>
+                                      ResultPage(image: _image!),
+                                ),
+                              );
+                            },
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: Color.fromRGBO(50, 50, 50, 1)),
+                            child: const Text(
+                              "Analyze",
+                              style: TextStyle(fontSize: 25),
+                            ),
+                          ),
                   ),
                 ],
               ),
